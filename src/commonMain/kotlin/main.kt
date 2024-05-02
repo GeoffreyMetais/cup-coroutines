d7828c6e39dee34a7a9ecd804cba7a1869442a43
Geoffrey Metais <gmetais@deezer.com>
2024-05-02 15:03:30 +0200

    Add Logo

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,16 +1,28 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cup_presentation_coroutines.generated.resources.Logo
+import cup_presentation_coroutines.generated.resources.Res
 import net.kodein.cup.*
 import net.kodein.cup.laser.laser
 import net.kodein.cup.speaker.speakerWindow
 import net.kodein.cup.ui.cupScaleDown
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import org.kodein.emoji.compose.EmojiService
 import slides.*
 
 
+@OptIn(ExperimentalResourceApi::class)
 fun main() = cupApplication(
     title = "Coroutines basics"
 ) {
@@ -40,6 +52,13 @@ fun main() = cupApplication(
                 LocalContentColor provides MaterialTheme.colors.onBackground
             ) {
                 slidesContent()
+                Image(
+                    painter = painterResource(Res.drawable.Logo),
+                    contentDescription = "Deezer logo",
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .size(64.dp)
+                    )
             }
         }
     }
